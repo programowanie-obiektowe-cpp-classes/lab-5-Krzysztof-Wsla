d9a226fe8f04b3798f203754a13eed6bab8a1dc5
Krzysztof Wszoła <krzyzak864@gmail.com>
2025-11-27 11:33:28 +0100

    p1

--- foo.hpp
+++ foo.hpp
@@ -8,5 +8,19 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     // Twoja implementacja tutaj
-    return {};
+    std::vector< char > lista;
+
+    for (auto v = people.rbegin(); v != people.rend(); ++v)
+    { 
+        v->birthday();
+        if (v->isMonster())
+        {
+            lista.push_back('y');
+        }
+        else
+        {
+            lista.push_back('n');
+        }
+    }
+    return lista;
 }
